21d0df4c4cf4a1cf05c5464ede55d291a5320681
Sayak Banerjee <69157316+gits-up@users.noreply.github.com>
2024-04-26 16:17:11 +0530

    update

---
+++ ecommerce/src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { SaleComponent } from './pages/sale/sale.component';
+
+export const routes: Routes = [
+    {
+        path: 'products',
+        component: HomeComponent
+    },
+
+    {
+        path: 'cart',
+        component: CartComponent
+    },
+
+    {
+        path: 'sale',
+        component: SaleComponent
+    }
+];
